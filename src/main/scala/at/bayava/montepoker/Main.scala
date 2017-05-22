9e0138441ca32769ba92e905c704a394ebd9e33f
philba <p.bayer@freemail.hu>
2017-05-22 22:35:24 +0200

    Instructions

--- src/main/scala/at/bayava/montepoker/Main.scala
+++ src/main/scala/at/bayava/montepoker/Main.scala
@@ -10,6 +10,12 @@ import scala.io.StdIn
 object Main {
 
 	def main(args: Array[String]): Unit = {
+		println("Usage")
+		println("Input your hand, the card pool, the number of players and number of simulations in the following format")
+		println("[Card]{2} [Card]{2} \\d \\d")
+		println("Where [Cards] is [1-9JQKA][CDHS]")
+		//		println("Where [Cards] is [1-9J(ack)Q(ueen)K(ing)A(ce)][C(lubs)D(iamonds)H(earts)S(pades)]")
+
 		var ok = true
 		val clReader = new ClReader()
 		while (ok) {
