ee179e0fcca884cf5d1caf721eb5429b0069a0b2
philba <p.bayer@freemail.hu>
2017-05-25 16:00:24 +0200

    to string, parsing of string

--- src/main/scala/at/bayava/montepoker/model/Hand.scala
+++ src/main/scala/at/bayava/montepoker/model/Hand.scala
@@ -4,12 +4,12 @@ package at.bayava.montepoker.model
 	* Created by philba on 5/22/17.
 	*/
 class Hand(val cards: Seq[Card]) {
-
 	def +(card: Card*): Hand = new Hand(cards ++ card)
+
 
 	def -(card: Card*): Hand = new Hand(cards diff card)
 
-	def -(card: Hand.Pair*): Hand = new Hand(cards diff card)
+	def --(card: Hand.Pair*): Hand = new Hand(cards diff card)
 
 	def highCard: Card = cards.max
 
@@ -20,10 +20,18 @@ class Hand(val cards: Seq[Card]) {
 			.map(pair => (pair.head, pair.last))
 	}
 
+	override def toString: String = "Hand(" + cards.mkString(", ") + ")"
 }
 
 object Hand {
+	private val cardRegex = "\\w{1,2}[CDHS]".r
 	type Pair = (Card, Card)
+
+	def apply(s: String): Hand = {
+		val cards = cardRegex.findAllIn(s)
+			.map(m => Card(m))
+		new Hand(cards.toSeq)
+	}
 
 	implicit object PairOrdering extends Ordering[Hand.Pair] {
 		override def compare(x: Hand.Pair, y: Hand.Pair): Int = x._1 compareTo y._1
