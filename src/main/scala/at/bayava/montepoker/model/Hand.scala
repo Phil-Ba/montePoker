b3e9bae08ea146ee7996f7181354ca60eeaf86db
philba <p.bayer@freemail.hu>
2017-05-25 13:24:12 +0200

    Utitlity methods and ordering

--- src/main/scala/at/bayava/montepoker/model/Hand.scala
+++ src/main/scala/at/bayava/montepoker/model/Hand.scala
@@ -5,6 +5,29 @@ package at.bayava.montepoker.model
 	*/
 class Hand(val cards: Seq[Card]) {
 
-	def +(card: Card): Hand = new Hand(cards :+ card)
+	def +(card: Card*): Hand = new Hand(cards ++ card)
+
+	def -(card: Card*): Hand = new Hand(cards diff card)
+
+	def -(card: Hand.Pair*): Hand = new Hand(cards diff card)
+
+	def highCard: Card = cards.max
+
+	def pairs: Iterable[Hand.Pair] = {
+		cards.groupBy(_.value)
+			.values
+			.withFilter(_.length == 2)
+			.map(pair => (pair.head, pair.last))
+	}
 
 }
+
+object Hand {
+	type Pair = (Card, Card)
+
+	implicit object PairOrdering extends Ordering[Hand.Pair] {
+		override def compare(x: Hand.Pair, y: Hand.Pair): Int = x._1 compareTo y._1
+	}
+
+}
+
