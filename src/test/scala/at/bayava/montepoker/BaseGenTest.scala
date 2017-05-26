c9baef06672495d43f4ef504b2578fd64c9a53eb
philba <p.bayer@freemail.hu>
2017-05-26 22:33:04 +0200

    no shrinking. workaround for pick bug. more gens.

--- src/test/scala/at/bayava/montepoker/BaseGenTest.scala
+++ src/test/scala/at/bayava/montepoker/BaseGenTest.scala
@@ -1,15 +1,20 @@
 package at.bayava.montepoker
 
-import at.bayava.montepoker.model.{Card, CardValues, Suites}
-import org.scalacheck.Gen
+import at.bayava.montepoker.model.{Card, CardValues, Hand, Suites}
+import org.scalacheck.{Gen, Shrink}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FunSpec, Matchers}
+
+import scala.util.Random
 
 /**
 	* Created by philba on 5/22/17.
 	*/
 trait BaseGenTest extends FunSpec with Matchers with GeneratorDrivenPropertyChecks {
 
+	implicit val noShrinkHand: Shrink[Hand] = Shrink.shrinkAny
+	implicit val noShrinkSeqCard: Shrink[Seq[Card]] = Shrink.shrinkAny
+	implicit val noShrinkCard: Shrink[Card] = Shrink.shrinkAny
 
 }
 
@@ -46,7 +51,21 @@ object CardGens {
 		(c, new Card(c.value, s._2))
 	}) suchThat (cp => cp._1.suite != cp._2.suite)
 
-	val nOfCardsGen = Gen.pick(_: Int, cardStack)
+	val nOfCardsGen = Gen.pick(_: Int, Random.shuffle(cardStack))
 
-	val handGen = nOfCardsGen(5)
+	val nOfCardsGenNoDuplicates = nOfCardsGen(_: Int) suchThat (_.groupBy(_.value)
+		.mapValues(_.length)
+		.values
+		.max == 1)
+
+	val nOfCardsGenNoDuplicates2 = nOfCardsGen(_: Int).withFilter(_.groupBy(_.value)
+		.mapValues(_.length)
+		.values
+		.max == 1)
+
+	val handOfNGen = nOfCardsGen(_: Int).map(new Hand(_))
+
+	val handOfNGenNoDuplicates = nOfCardsGenNoDuplicates(_: Int).map(new Hand(_))
+
+	val handGen = handOfNGen(5)
 }
